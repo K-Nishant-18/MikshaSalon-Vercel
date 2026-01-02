fb6bfe97f762560e321a3950bc1e97d6f05eee06
KUMAR NISHANT <me.knishant@gmail.com>
2026-01-02 16:38:38 +0530

    Implemented PWA

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Register service worker for PWA
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js', { scope: '/' })
+            .catch(() => {
+                // Service worker registration failed, app will still work
+            });
+    });
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
